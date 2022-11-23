2cc7381e286fa9f0d77940219c3a9d330c85be6f
rbadi76 <rbadi76@gmail.com>
2022-11-23 17:02:40 +0000

    Temporary files for learning, to be removed later.

---
+++ myfiles/scrachPad.py
@@ -0,0 +1,16 @@
+from collections import defaultdict, OrderedDict
+
+class Person:
+
+    def __init__(self):
+        self.name = "Róbert"
+        self.age = 46
+        self.properties: dict[int, str] = { 0: "This", 1: "is", 2: "a", 3: "dictionary"}
+
+
+    def __str__(self):
+        return "Hi, my name is " + self.name + " and I am " + str(self.age) + " old!"
+
+myVar = Person()
+print(myVar)
+print(myVar.properties)
--- myfiles/somecode.py
+++ myfiles/somecode.py
@@ -5,7 +5,7 @@ from reynir import Greynir
 g = Greynir()
 #sent = g.parse_single("Talið er að hún hafi verið á leiðinni til Íslands ásamt öðrum álftum þegar slysið varð.")
 
-fulltext = "Merki um stríðsglæpi Rússa sé að finna í nánast hverri einustu borg sem þeir hafa yfirgefið, segir Óskar Hallgrímsson ljósmyndari í Kænugarði. „Maður er alveg farinn að finna fyrir að fyrir að taugakerfið  er ekkert sérstaklega gott eftir að hafa verið með það útþanið í tæplega þrjá mánuði. En ég og kona mín erum ágætir félagar og styðjum hvort annað vel í þessu“ sagði Óskar í morgunþætti Rásar tvö í morgun. „Stríðið skilur eftir sig svakalegt sár hjá fólki og reiðin er orðin mikil. Rússar eru hættir að heyja stríð eins og maður myndi halda að hefðbundið stríð væri.  Nú ráðast þeir á borgarleg skotmörk og þær borgir sem Rússar hafa hertekið og yfirgefið eru í rúst. Það virðist vera merki um stríðsglæpi og nauðganir á nánast hverjum einasta stað sem þeir hafa verið ár.“"
+'''fulltext = "Merki um stríðsglæpi Rússa sé að finna í nánast hverri einustu borg sem þeir hafa yfirgefið, segir Óskar Hallgrímsson ljósmyndari í Kænugarði. „Maður er alveg farinn að finna fyrir að fyrir að taugakerfið  er ekkert sérstaklega gott eftir að hafa verið með það útþanið í tæplega þrjá mánuði. En ég og kona mín erum ágætir félagar og styðjum hvort annað vel í þessu“ sagði Óskar í morgunþætti Rásar tvö í morgun. „Stríðið skilur eftir sig svakalegt sár hjá fólki og reiðin er orðin mikil. Rússar eru hættir að heyja stríð eins og maður myndi halda að hefðbundið stríð væri.  Nú ráðast þeir á borgarleg skotmörk og þær borgir sem Rússar hafa hertekið og yfirgefið eru í rúst. Það virðist vera merki um stríðsglæpi og nauðganir á nánast hverjum einasta stað sem þeir hafa verið ár.“"
 
 sents = fulltext.split(".")
 
@@ -20,8 +20,9 @@ for sent in sents:
         print("Sent " + str(i) + " is ambiguous: " + str(parsedSent._tree.is_ambiguous))
     else:
         print("Could not parse sentence. Error index: " + str(parsedSent.err_index))
-    i += 1
+    i += 1'''
 
+sent = g.parse_single("Umframfiskur ratar á diska fátæka fólksins")
 print("Done")
 
 #print(sent.tree.view)
